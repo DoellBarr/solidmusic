36fe76f505028e0824e51eca98325ebd56c0a27f
Shohih Abdul <shohih242@gmail.com>
2021-11-10 15:32:20 +0700

    fix code error bug

--- base/player.py
+++ base/player.py
@@ -2,10 +2,11 @@ from typing import Dict
 
 from pyrogram import types
 from pytgcalls import idle
-from pytgcalls.types import Update
 
 from .music_base import MusicPlayer
 from .video_base import VideoPlayer
+from .bot_base import pyro_bot
+from .client_base import call_py
 
 
 class Methods(MusicPlayer, VideoPlayer):
@@ -13,26 +14,6 @@ class Methods(MusicPlayer, VideoPlayer):
 
 
 class Player(Methods):
-    def __init__(self):
-
-        super().__init__()
-        self._play = super()._play
-        self._stream = super()._stream
-        self._bot = super()._bot
-
-        call = super()._call
-        playlist = super()._playlist
-
-        @call.on_stream_end()
-        async def _(_, update: Update):
-            chat_id = update.chat_id
-            if len(playlist[chat_id]) > 1:
-                playlist[chat_id].pop(0)
-                yt_url = playlist[chat_id][0]["yt_url"]
-                return await self.stream_change(chat_id, yt_url)
-            await call.leave_group_call(chat_id)
-            del playlist[chat_id]
-
     async def play_or_stream(self, cb: types.CallbackQuery, result: Dict):
         user_id = result["user_id"]
         title = result["title"]
@@ -40,24 +21,24 @@ class Player(Methods):
         yt_url = result["yt_url"]
         yt_id = result["yt_id"]
         stream_type = result["stream_type"]
-
         if stream_type == "music":
-            return await self._play(cb, user_id, title, duration, yt_url, yt_id)
+            return await self.play(cb, user_id, title, duration, yt_url, yt_id)
         if stream_type == "stream":
             quality = result["quality"]
-            return await self._stream(
+            return await self.stream(
                 cb, user_id, title, duration, yt_url, yt_id, quality
             )
 
-    async def start(self):
+    @staticmethod
+    async def start():
         print("[ INFO ] STARTING BOT CLIENT")
-        await self._bot.start()
+        await pyro_bot.start()
         print("[ INFO ] STARTING PyTgCalls CLIENT")
-        await self._call.start()
+        await call_py.start()
         print("[ INFO ] CLIENT RUNNING")
         await idle()
         print("[ INFO ] STOPPING BOT")
-        await self._bot.stop()
+        await pyro_bot.stop()
 
 
 player = Player()
