d9e4eb44c95e4bbb115caae0e4565956c4e65390
Shohih Abdul <shohih242@gmail.com>
2021-11-10 15:32:46 +0700

    add decorator function

---
+++ utils/functions/decorators.py
@@ -0,0 +1,21 @@
+from typing import Callable
+
+from pyrogram import types, Client
+from dB.database import db
+from base.bot_base import bot_client as bot
+
+
+def authorized_only(func: Callable) -> Callable:
+    async def wrapper(client: Client, message: types.Message):
+        if message.from_user.id in db.get_sudos(message.chat.id):
+            return await func(client, message)
+        admins = await message.chat.get_members(filter="administrators")
+        for admin in admins:
+            if not admin.can_manage_voice_chats:
+                return await bot.send_message(
+                    message,
+                    "need_privilege",
+                    reply_message=True
+                )
+            return await func(client, message)
+    return wrapper
