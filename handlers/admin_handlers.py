882ea9c70d7d00c87990db71e01e1bc1982095ee
Shohih Abdul <shohih242@gmail.com>
2021-11-10 13:23:16 +0700

    update feature

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -1,9 +1,11 @@
 from pyrogram import Client, filters, types
 from base.bot_base import bot_client as bot
 from base.player import player
+from utils.functions.decorators import authorized_only
 
 
 @Client.on_message(filters.command("pause"))
+@authorized_only
 async def pause(_, message: types.Message):
     chat_id = message.chat.id
     await player.change_streaming_status("pause", chat_id)
@@ -15,6 +17,7 @@ async def pause(_, message: types.Message):
 
 
 @Client.on_message(filters.command("resume"))
+@authorized_only
 async def resume_(_, message: types.Message):
     chat_id = message.chat.id
     await player.change_streaming_status("resume", chat_id)
@@ -26,6 +29,7 @@ async def resume_(_, message: types.Message):
 
 
 @Client.on_message(filters.command("skip"))
+@authorized_only
 async def skip_(_, message: types.Message):
     chat_id = message.chat.id
     toxt = await player.change_stream(chat_id)
@@ -37,6 +41,7 @@ async def skip_(_, message: types.Message):
 
 
 @Client.on_message(filters.command("vol"))
+@authorized_only
 async def change_vol_(_, message: types.Message):
     chat_id = message.chat.id
     vol = int(message.command[1])
@@ -50,6 +55,7 @@ async def change_vol_(_, message: types.Message):
 
 
 @Client.on_message(filters.command("end"))
+@authorized_only
 async def end_stream_(_, message: types.Message):
     chat_id = message.chat.id
     await player.end_stream(chat_id)
