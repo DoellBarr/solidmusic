9a040276d4aeef3ae0e400930ea6cec7b09d17a8
jokokendi <80366713+jokokendi@users.noreply.github.com>
2021-11-11 07:58:39 +0700

    Delete cbhelp.py

--- handlers/cbhelp.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import filters, Client
-from pyrogram.types import (
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    Message,
-)
-
-from base.bot_base import bot_client as bot
-from dB.lang_utils import get_message as gm
