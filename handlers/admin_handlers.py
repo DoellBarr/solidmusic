d1e36dfef1f005a58d446bf3c921f5a059339fd5
Shohih Abdul <shohih242@gmail.com>
2021-11-11 15:51:35 +0700

    try to add update command

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -1,7 +1,12 @@
+import sys
+
 from pyrogram import Client, filters, types
 from base.bot_base import bot_client as bot
 from base.player import player
+from configs import config
 from utils.functions.decorators import authorized_only
+from git import Repo
+from os import execle, system
 
 
 @Client.on_message(filters.command("pause"))
@@ -47,3 +52,20 @@ async def end_stream_(_, message: types.Message):
     if check_call:
         return await bot.send_message(message, "track_ended", reply_message=True)
     return await bot.send_message(message, "not_streaming", reply_message=True)
+
+
+@Client.on_message(filters.command("update") & filters.user(config.OWNER_ID))
+async def update_repo(_, message: types.Message):
+    msg = await message.reply("**processing...**")
+    repo = Repo().init()
+    if "origin" in repo.remotes:
+        origin = repo.remote("origin")
+    else:
+        origin = repo.create_remote("origin", "https://github.com/DoellBarr/solidmusic")
+    origin.fetch()
+    origin.pull()
+    system("pip3 install --no-cache-dir -r requirements.txt")
+    await msg.edit("**update finished, now deploying**")
+    args = [sys.executable, "main.py"]
+    execle(sys.executable, *args)
+    sys.exit()
