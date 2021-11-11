b7122524ef179d5dad55dfb07a8994b1ebfd657e
Shohih Abdul <shohih242@gmail.com>
2021-11-11 10:33:09 +0700

    add new feature for collect the thumbnail images

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from base.player import player
 from sys import version
+from os.path import exists
+from os import mkdir
 import asyncio
 
 py_ver = version.split()[0]
 
 
 def main():
+    if not exists("search"):
+        mkdir("search")
     if py_ver.startswith("3.9"):
         loop = asyncio.get_event_loop()
         loop.run_until_complete(player.start())
