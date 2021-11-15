3fc6d3a5998850456df968b85cc67543f75fb343
Shohih Abdul <shohih242@gmail.com>
2021-11-15 14:24:27 +0700

    Resolved #17 - add and del sudo now can mention a user

--- plugins/sudo_settings.py
+++ plugins/sudo_settings.py
@@ -9,11 +9,16 @@ from utils.functions.decorators import authorized_only
 async def add_sudo_(_, message: types.Message):
     chat_id = message.chat.id
     reply = message.reply_to_message
+    user = message.command[1]
     if reply:
         user_id = reply.from_user.id
         toxt = db.add_sudo(chat_id, user_id)
         return await bot.send_message(message, toxt, reply_message=True)
-    user_id = int(message.command[1])
+    if user.startswith("@"):
+        user_id = (await message.chat.get_member(user)).user.id
+        toxt = db.add_sudo(chat_id, user_id)
+        return await bot.send_message(message, toxt, user, reply_message=True)
+    user_id = int(user)
     toxt = db.add_sudo(chat_id, user_id)
     return await bot.send_message(message, toxt, str(user_id), reply_message=True)
 
@@ -23,10 +28,15 @@ async def add_sudo_(_, message: types.Message):
 async def del_sudo_(_, message: types.Message):
     chat_id = message.chat.id
     reply = message.reply_to_message
+    user = message.command[1]
     if reply:
         user_id = reply.from_user.id
         toxt = db.del_sudo(chat_id, user_id)
         return await bot.send_message(message, toxt, reply_message=True)
-    user_id = int(message.command[1])
+    if user.startswith("@"):
+        user_id = (await message.chat.get_member(user)).user.id
+        toxt = db.del_sudo(chat_id, user_id)
+        return await bot.send_message(message, toxt, user, reply_message=True)
+    user_id = int(user)
     toxt = db.del_sudo(chat_id, user_id)
     return await bot.send_message(message, toxt, str(user_id), reply_message=True)
