1887f61bc34d48fe43aa5bf5886f9ed2477f8676
Shohih Abdul <shohih242@gmail.com>
2021-11-15 19:44:44 +0700

    update language formats

--- dB/lang_utils.py
+++ dB/lang_utils.py
@@ -13,6 +13,7 @@ lang_flags = {
     "en": f"{emoji.FLAG_UNITED_STATES} English",
     "id": f"{emoji.FLAG_INDONESIA} Indonesia",
     "grl": f"{emoji.FLAG_INDONESIA} Girl",
+    "jv": f"{emoji.FLAG_INDONESIA} Javanese",
     "pt": f"{emoji.FLAG_PORTUGAL} Portuguese",
     "ta": f"{emoji.FLAG_INDIA} Tamil"
 }
