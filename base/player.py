6fd4b3906e35c85d0f5a88ccd75c8b65ee64056e
Shohih Abdul <shohih242@gmail.com>
2021-11-16 21:55:13 +0700

    fix the `ChannelInvalid` error

--- base/player.py
+++ base/player.py
@@ -2,7 +2,7 @@ import asyncio
 from typing import Dict
 
 from pyrogram import types
-from pyrogram.errors import UserNotParticipant
+from pyrogram.errors import UserNotParticipant, ChannelInvalid
 from pytgcalls import idle
 
 from .music_base import MusicPlayer
@@ -28,6 +28,12 @@ class Player(Methods):
                 cb, user_id, title, duration, yt_url, yt_id, quality
             )
 
+    @staticmethod
+    async def promote_client(chat_id, cb, client_user_id):
+        await user.join_chat(chat_id)
+        await cb.message.chat.promote_member(client_user_id, can_manage_voice_chats=True)
+        await asyncio.sleep(2)
+
     async def play_or_stream(self, cb: types.CallbackQuery, result: Dict):
         user_id = result["user_id"]
         title = result["title"]
@@ -40,9 +46,11 @@ class Player(Methods):
         try:
             await self.join_stream(stream_type, cb, chat_id, user_id, title, duration, yt_url, yt_id)
         except UserNotParticipant:
-            await user.join_chat(chat_id)
-            await cb.message.chat.promote_member(client_user_id, can_manage_voice_chats=True)
-            await asyncio.sleep(2)
+            await self.promote_client(chat_id, cb, client_user_id)
+            await self.join_stream(stream_type, cb, chat_id, user_id, title, duration, yt_url, yt_id)
+        except ChannelInvalid:
+            await self.promote_client(chat_id, cb, client_user_id)
+            await self.create_call(chat_id)
             await self.join_stream(stream_type, cb, chat_id, user_id, title, duration, yt_url, yt_id)
 
     async def start(self):
