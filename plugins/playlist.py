b2698bafd9a39b7b554fe3c7a9562fdfa313d781
Shohih Abdul <shohih242@gmail.com>
2021-11-16 12:32:04 +0700

    Resolved #27 - add playlist handler

---
+++ plugins/playlist.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, filters, types
+
+from base.player import player
+from dB.lang_utils import get_message as gm
+from utils.functions.decorators import authorized_only
+
+
+@Client.on_message(filters.command("playlist"))
+@authorized_only
+async def playlist_(client: Client, message: types.Message):
+    bot_username = (await client.get_me()).username
+    chat_id = message.chat.id
+    current, queued = player.send_playlist(chat_id)
+    current_title = current["title"]
+    current_yt_info = f"https://t.me/{bot_username}?start=ytinfo_{current['yt_id']}"
+    current_stream_type = current["stream_type"]
+    if current and not queued:
+        text = (
+            f"**{gm(chat_id, 'now_streaming')}**:\n» [{current_title}]({current_yt_info}) | `{current_stream_type}`"
+        )
+        return await message.reply(text, disable_web_page_preview=True)
+    if current and queued:
+        text = (
+            f"**{gm(chat_id, 'now_streaming')}**:\n» [{current_title}]({current_yt_info}) | `{current_stream_type}`\n\n"
+            f"**{gm(chat_id, 'playlist')}**\n"
+        )
+        for count, queues in enumerate(queued, start=1):
+            queue_title = queues["title"]
+            queue_yt_info = f"https://t.me/{bot_username}?start=ytinfo_{queues['yt_id']}"
+            queue_stream_type = queues["stream_type"]
+            text += f"**#{count}** - [{queue_title}]({queue_yt_info}) | `{queue_stream_type}`\n"
+        return await message.reply(text, disable_web_page_preview=True)
+    return await message.reply(gm(chat_id, "no_playlists"))
