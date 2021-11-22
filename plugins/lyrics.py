1f335607984db951c6ca9cbc76231824bca65af8
Shohih Abdul <50512936+DoellBarr@users.noreply.github.com>
2021-11-22 08:10:50 +0700

    Update lyrics.py

--- plugins/lyrics.py
+++ plugins/lyrics.py
@@ -7,15 +7,12 @@ from dB.lang_utils import get_message as gm
 
 @Client.on_message(filters.command(["lyrics"]) & ~filters.edited)
 async def lyrics(_, message : Message):
-    query = " ".join(message.command[1:])
-    chid = message.chat.id
     if len(message.command) < 2:
         return await message.reply_text(gm(chid, "ly_key"))
-        
+    query = " ".join(message.command[1:])
+    chid = message.chat.id
     lek = await message.reply_text(gm(chid, "searching"))
-
     req = requests.get(f"https://apis.xditya.me/lyrics?song={query}").json()
     lyric = req['lyrics']
     name = req['name']
-
     await lek.edit(gm(chid, "lyrik")).format({name},{lyric})
