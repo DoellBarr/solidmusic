8bbfae17f4f8d61fcfe3d83942ed0621d08ab43b
Shohih Abdul <shohih242@gmail.com>
2021-11-28 05:21:46 +0700

    New Update - Add helpers module

---
+++ plugins/helper.py
@@ -0,0 +1,22 @@
+from . import paginate_module
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+from database.lang_utils import get_message as gm, get_message
+
+
+@Client.on_message(filters.command("help"))
+async def help_cmds_(_, message: Message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    keyboard = paginate_module(chat_id, user_id)
+    await message.reply(
+        get_message(chat_id, "here_all_commands"),
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
+
+
+__cmds__ = ["help"]
+__help__ = {
+    "help": "help_help"
+}
