ff3a1e395016354e7f4947c0185e9104b7cd281e
Shohih Abdul <shohih242@gmail.com>
2022-03-30 14:41:18 +0700

    Change language file to yaml, add PyYaml and aiosqlite to requirements.txt

--- solidmusic/database/lang_utils.py
+++ solidmusic/database/lang_utils.py
@@ -1,19 +1,18 @@
-import json
+import yaml
+from solidmusic.database.chat_db import chat_db
 from os.path import join, dirname, realpath
 from os import listdir
 from typing import Dict
 
-from .chat_db import chat_db
-
-lang_dir = join(dirname(realpath(__file__)), "lang")
+lang_dir = join(dirname(realpath(__file__)), "langs")
 list_lang = []
 lang: Dict[str, Dict[str, str]] = {}
 
 for file in listdir(lang_dir):
-    if file.endswith(".json"):
+    if file.endswith(".yaml"):
         lang_code = file[:-5]
         list_lang.append(lang_code)
-        lang[lang_code] = json.load(open(join(lang_dir, file), encoding="UTF-8"))
+        lang[lang_code] = yaml.full_load(open(join(lang_dir, file)))
 
 
 async def gm(chat_id: int, key: str, format_key=None) -> str:
