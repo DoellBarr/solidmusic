567d1bcb50067460ad69fa6e6a991c060072bc76
Shohih Abdul <shohih242@gmail.com>
2022-04-05 09:35:11 +0700

    add player module

---
+++ solidmusic/core/player.py
@@ -0,0 +1,63 @@
+import sys
+import solidmusic.core
+
+from .types import CallbackQuery
+from .telegram_calls import TelegramPlayer
+from .youtube_calls import YoutubePlayer
+
+from pyrogram import idle
+from pyrogram.errors import UserAlreadyParticipant
+
+from os import path
+from shutil import rmtree
+
+
+
+class Player(TelegramPlayer, YoutubePlayer):
+    async def join_call(
+        self,
+        cb: CallbackQuery,
+        title: str,
+        duration: str,
+        yt_url: str,
+        yt_id: str,
+        stream_type: str
+    ):
+        return await self.stream(
+            cb, title, duration, yt_url, yt_id, stream_type
+        )
+
+    async def start_stream(self, cb: CallbackQuery, results: dict[str, str]):
+        title, duration, yt_url, yt_id, stream_type = results.values()
+        return await self.join_call(
+            cb, title, duration, yt_url, yt_id, stream_type
+        )
+
+    async def run(self):
+        await self.db.init()
+        print("[+] START BOT CLIENT")
+        await self.bot.start()
+        print("[+] LOAD ALL MODULES")
+        # todo make load_module function
+        print("[+] Getting Bot Username".upper())
+        bot_username = await self.bot.get_username()
+        solidmusic.core.username = bot_username
+        print("[+] START PyTgCalls CLIENT")
+        await self.call.start()
+        try:
+            await self.user.join_chat("SolidProjectsReborn")
+        except UserAlreadyParticipant:
+            pass
+        print("[+] CLIENT RUNNING")
+        await idle()
+        print("[+] STOPPING BOT")
+        await self.db.disconnect()
+        if path.exists("downloads"):
+            rmtree("downloads")
+        if path.exists("search"):
+            rmtree("search")
+        await self.bot.stop()
+        return sys.exit()
+
+
+player = Player()
