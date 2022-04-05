d7b70a57e2e4ba9bbf6db01644673d9252a86399
Shohih Abdul <shohih242@gmail.com>
2022-04-05 09:33:58 +0700

    add some variable to init

--- solidmusic/core/__init__.py
+++ solidmusic/core/__init__.py
@@ -1,1 +1,9 @@
+from datetime import datetime as dt
+
 __version__ = "2.0.0"
+__author__ = "Shohih Abdul"
+__license__ = "MIT"
+__copyright__ = f"Copyright (c) {dt.now().year} Shohih Abdul"
+__all__ = ["__version__", "__author__", "__license__", "__copyright__"]
+
+username = ""
--- solidmusic/core/types/__init__.py
+++ solidmusic/core/types/__init__.py
@@ -1,2 +1,8 @@
 from .message import Message
 from .callback_query import CallbackQuery
+
+
+__all__ = [
+    'Message',
+    'CallbackQuery'
+]
